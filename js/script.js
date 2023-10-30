4bf28f4977a924cfe84e0cce41667d178e9c1bf0
Adrian Kaszuba <adikaszi@protonmail.com>
2023-10-30 15:17:42 +0100

    6.3.7

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 const titleClickHandler = function(event){
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
+  /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
 
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
 
   for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
   }
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector)
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+  console.log('targetArticle:', targetArticle);
 
-  /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
